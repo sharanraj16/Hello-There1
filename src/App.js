97dc0793ee307de4ca76d4569ff8ebb3a02f8134
sharanraj16 <sharanraj.sivakumar@students.codeinstitute.net.test-google-a.com>
2025-04-20 16:46:41 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <NavBar />
     </div>
   );
 }
