4464f84344cf214dfdc340d83d3d022b6ac039d4
sharanraj16 <sharanraj.sivakumar@students.codeinstitute.net.test-google-a.com>
2025-04-21 16:35:32 +0200

    updated signin ans signup

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://hello-there-2cedc687affc.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
